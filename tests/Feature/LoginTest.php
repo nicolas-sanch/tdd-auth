0d07c798e6a8646b797a1dc6202e7a360870c356
nicolas <nicolas.sanch@protonmail.com>
2022-04-12 13:53:11 +0200

    First test created

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function when_a_user_gives_his_credentials_he_is_authenticated()
+    {
+        $user = User::factory()->create();
+
+        $credentials = [
+            'email' => $user->email,
+            'password' => 'password'
+        ];
+
+        $this->post('/login', $credentials)->assertRedirect(route('dashboard'));
+
+        $this->assertEquals(auth()->id(), $user->id);
+    }
+}
